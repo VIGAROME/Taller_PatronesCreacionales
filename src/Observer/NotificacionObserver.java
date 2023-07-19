3a64e4817b8c761fae7da1739abfcdb11aa816d5
Victor Garcia <vigarome@espol.edu.ec>
2023-07-19 17:23:06 -0500

    add observers

---
+++ src/Observer/NotificacionObserver.java
@@ -0,0 +1,11 @@
+package Observer;
+
+public class NotificacionObserver implements Observer {
+
+	@Override
+	public void actualizar(String msj) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Sistema/Tarea.java
@@ -0,0 +1,11 @@
+package Sistema;
+import java.util.ArrayList;
+import java.util.List;
+
+import Observer.Observer;
+
+public abstract class Tarea {
+	
+	private List<Observer> observadores = new ArrayList<>();
+
+}
